17bd03cb1c0fdcc32e787fb8d37e9689d6d9db59
Yoshiya Hinosawa <stibium121@gmail.com>
2014-05-24 19:46:18 +0900

    Initial Commit

---
+++ StrawServiceHttp/StrawServiceHttp.m
@@ -0,0 +1,13 @@
+//
+//  StrawServiceHttp.m
+//  StrawServiceHttp
+//
+//  Created by kt3k on 2014/05/24.
+//  Copyright (c) 2014年 kt3k.com. All rights reserved.
+//
+
+#import "StrawServiceHttp.h"
+
+@implementation StrawServiceHttp
+
+@end
