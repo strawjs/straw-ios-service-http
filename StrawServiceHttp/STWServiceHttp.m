fba1c60eaa39db67c6a9e7ce5b1584c171bcaa8e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-06-04 22:56:57 +0900

    Add the API doc of 'get' method

--- StrawServiceHttp/STWServiceHttp.m
+++ StrawServiceHttp/STWServiceHttp.m
@@ -13,10 +13,19 @@
 }
 
 
+/**
+ @serviceMethod
+ Perform a http GET request to the url
+
+ @param params The parameters for the method call. params[@"url"] is the url. params[@"timeout"] is the timeout in seconds. params[@"charset"] is the charset to use in decoding the response text.
+ @param context The Service Call context of the Method.
+
+ */
 - (void)get:(NSDictionary *)params withContext:(id<STWServiceCallContext>)context
 {
     NSString *urlString = params[@"url"];
     NSNumber *timeout = params[@"timeout"];
+    NSString *charset = params[@"charset"];
 
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]
                                                   cachePolicy:NSURLRequestUseProtocolCachePolicy
